85180a33635604000588396f6a6baed949e8dcd0
shintojoseph1234 <shinto.joseph@valustat.co.uk>
2019-11-04 01:03:48 +0530

    Initial commit

--- autocomplete/settings.py
+++ autocomplete/settings.py
@@ -147,7 +147,6 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
